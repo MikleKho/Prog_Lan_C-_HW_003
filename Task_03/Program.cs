f478921590dd0618eebe60a5072bd0e9cf1d0a3d
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-16 19:12:15 +0300

    Решение Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,3 +2,23 @@
 // выдаёт таблицу кубов чисел от 1 до N. 
 // 3 -> 1, 8, 27.
 // 5 -> 1, 8, 27, 64, 125
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+
+
+void Cube_numbers(int Last_number)
+{
+    System.Console.Write("Кубы чисел от 1 до введенного включительно:  ");
+    for (int i = 1; i <= Last_number; i++)
+    {
+        System.Console.Write($"{Math.Pow(i, 3)}  ");
+    }
+}
+
+int value_01 = Prompt("Введите число -> ");
+Cube_numbers(value_01);
